e21d9bcef88ef7d89f889d831af99e228f70c16a
bhartiharinkhede44 <bhartiharinkhede44@gmail.com>
2023-09-25 09:24:42 +0530

    contact-list

---
+++ src/views/App/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import "./App.css"
+import contactlist from "../../data/contact-list"
+import ContactCard from '../../component/ContactCard/ContactCard';
+function App() {
+    const [contacts, setContacts] = useState(contactlist);
+    const [serachTerm, setSearchTerm] = useState('')
+    useEffect(() => {
+        const filteredContacts = contactlist.filter((contact) => {
+            const name = contact.name.toLocaleLowerCase();
+            const mobile = contact.mobile.toString();
+            const query = serachTerm.toLocaleLowerCase();
+            return (name.includes(query) || mobile.includes(query))
+        })
+        setContacts(filteredContacts);
+    }, [serachTerm]
+    )
+    return (
+        <>
+            <h1 className='heading'>Contact-List</h1>
+            <input type='text'
+                placeholder='search'
+                className='search'
+                value={serachTerm}
+                onChange={(e) => { setSearchTerm(e.target.value) }} />
+            <div>
+                {contacts.map((contact, index) => {
+                    const { name, mobile } = contact;
+                    return <ContactCard key={index} name={name} mobile={mobile} />
+                })}
+            </div>
+            {
+                contacts.length === 0 ? <h2 className='heading'>No contact found</h2> : null
+            }
+        </>
+    )
+}
+
+export default App
